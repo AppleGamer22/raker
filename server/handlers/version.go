1cac177995a729a3919ac8aeb97653634f58908d
Omri Bornstein <omribor@gmail.com>
2022-05-28 11:11:45 +1000

    version endpoint

--- server/handlers/version.go
+++ server/handlers/version.go
@@ -1,12 +1,23 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/AppleGamer22/rake/shared"
 )
 
+type version struct {
+	Version string `json:"version"`
+	Hash    string `json:"hash"`
+}
+
 func Version(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprint(writer, shared.Version)
+	jsonPayload := version{shared.Version, shared.Hash}
+	data, err := json.Marshal(jsonPayload)
+	if err != nil {
+		http.Error(writer, "could not process version information", http.StatusInternalServerError)
+	}
+	fmt.Fprint(writer, string(data))
 }
