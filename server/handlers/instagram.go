582ca3345dfb4646f1ea6821d68fcef7992eab8a
Omri Bornstein <omribor@gmail.com>
2022-06-17 21:01:41 +1000

    initial html

--- server/handlers/instagram.go
+++ server/handlers/instagram.go
@@ -1,11 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
 
 func Instagram(writer http.ResponseWriter, request *http.Request) {
 
 }
 
 func InstagramPage(writer http.ResponseWriter, request *http.Request) {
-
+	tmpl, err := template.ParseFiles(filepath.Join("templates", "instagram.html"))
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	tmpl.ExecuteTemplate(writer, "instagram", nil)
 }
