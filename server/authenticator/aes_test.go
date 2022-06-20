016c2121501206d870545775d6c3cd64db10ee9f
Omri Bornstein <omribor@gmail.com>
2022-06-20 13:16:05 +1000

    AES stack decoupled from Authenticator

--- server/authenticator/aes_test.go
+++ server/authenticator/aes_test.go
@@ -3,15 +3,19 @@ package authenticator_test
 import (
 	"testing"
 
+	"github.com/AppleGamer22/rake/server/authenticator"
 	"github.com/stretchr/testify/assert"
 )
 
-const expectedPlainText = "an important secret"
+const (
+	expectedPlainText = "an important secret"
+	key               = "key"
+)
 
 func TestAES(t *testing.T) {
-	ct, err := testAuthenticator.Encrypt(expectedPlainText)
+	ct, err := authenticator.Encrypt(key, expectedPlainText)
 	assert.NoError(t, err)
-	pt, err := testAuthenticator.Decrypt(ct)
+	pt, err := authenticator.Decrypt(key, ct)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedPlainText, pt)
 }
