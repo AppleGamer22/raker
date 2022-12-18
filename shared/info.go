0ed84d42074e7143e5326c871bbeb475f4115170
Omri Bornstein <omribor@gmail.com>
2022-12-18 13:04:43 +1100

    fetching latest user agent string

--- shared/info.go
+++ shared/info.go
@@ -1,7 +1,51 @@
 package shared
 
-const (
-	Version   = "development"
-	Hash      = "development"
-	UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"runtime"
+	"strings"
 )
+
+const (
+	Version = "development"
+	Hash    = "development"
+)
+
+var UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+
+type userAgentData struct {
+	Data []struct {
+		UserAgent string `json:"ua"`
+	} `json:"data"`
+}
+
+func init() {
+	response, err := http.Get("https://www.useragents.me/api")
+	if err != nil {
+		log.Println("could not retrieve the latest user agent")
+		return
+	}
+	defer response.Body.Close()
+
+	var data userAgentData
+	if err := json.NewDecoder(response.Body).Decode(&data); err != nil || len(data.Data) == 0 {
+		log.Println("could not retrieve the latest user agent")
+		return
+	}
+
+	osName := runtime.GOOS
+	if osName == "darwin" {
+		osName = "mac"
+	}
+
+	for _, userAgent := range data.Data {
+		if strings.Contains(strings.ToLower(userAgent.UserAgent), osName) {
+			UserAgent = userAgent.UserAgent
+			log.Println("using user agent", UserAgent)
+			return
+		}
+	}
+	log.Println("could not retrieve the latest user agent, using default")
+}
