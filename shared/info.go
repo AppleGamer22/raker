e8ac0db3b69e2cb0a64fcfdadf2307567cf21e5c
Omri Bornstein <omribor@gmail.com>
2024-06-26 19:00:25 +1000

    user agent fetching
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- shared/info.go
+++ shared/info.go
@@ -2,7 +2,9 @@ package shared
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+	"regexp"
 	"runtime"
 	"strings"
 	"time"
@@ -15,12 +17,13 @@ const (
 	Hash    = "development"
 )
 
-var UserAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+var (
+	UserAgent      = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+	textareaRegExp = regexp.MustCompile(`<textarea class="form-control" rows="8">(.*?)</textarea>`)
+)
 
 type userAgentData struct {
-	Data []struct {
-		UserAgent string `json:"ua"`
-	} `json:"data"`
+	UserAgent string `json:"ua"`
 }
 
 func init() {
@@ -30,15 +33,27 @@ func init() {
 }
 
 func init() {
-	response, err := http.Get("https://www.useragents.me/api")
+	response, err := http.Get("https://www.useragents.me/")
 	if err != nil {
 		log.Error("could not retrieve the latest user agent")
 		return
 	}
 	defer response.Body.Close()
 
-	var data userAgentData
-	if err := json.NewDecoder(response.Body).Decode(&data); err != nil || len(data.Data) == 0 {
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	textarea := textareaRegExp.FindAllStringSubmatch(string(body), 1)
+	if len(textarea) == 0 {
+		log.Error("couldn't find latest user agent string")
+		return
+	}
+
+	var data []userAgentData
+	if err := json.Unmarshal([]byte(textarea[0][1]), &data); err != nil || len(data) == 0 {
 		log.Error("could not retrieve the latest user agent", "err", err)
 		return
 	}
@@ -48,8 +63,8 @@ func init() {
 		osName = "mac"
 	}
 
-	for _, userAgent := range data.Data {
-		if strings.Contains(strings.ToLower(userAgent.UserAgent), osName) {
+	for _, userAgent := range data {
+		if strings.Contains(strings.ToLower(userAgent.UserAgent), osName) && !strings.Contains(userAgent.UserAgent, "Samsung") {
 			UserAgent = userAgent.UserAgent
 			log.Infof("using user agent %s", UserAgent)
 			return
