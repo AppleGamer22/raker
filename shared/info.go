fa4ae381933248b40afd1572bea6de2f5f990fa1
Omri Bornstein <omribor@gmail.com>
2025-03-10 12:52:56 +1100

    version is written correctly
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- shared/info.go
+++ shared/info.go
@@ -12,12 +12,9 @@ import (
 	"github.com/charmbracelet/log"
 )
 
-const (
-	Version = "development"
-	Hash    = "development"
-)
-
 var (
+	Version        = "development"
+	Hash           = "development"
 	UserAgent      = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 	textareaRegExp = regexp.MustCompile(`<textarea class="form-control" rows="8">(.*?)</textarea>`)
 )
