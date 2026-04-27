f740c9515b923114bf4fd07d029cb464bf4e1f60
Omri Bornstein <omribor@gmail.com>
2026-04-27 16:55:57 +1000

    header logo
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,13 @@
+import { MatchRoute } from "@tanstack/react-router";
 import { SidebarIcon } from "lucide-react";
 
 import { RakerLogo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import { InstagramIcon } from "@/components/ui/svgs/instagram";
+import { TikTokIcon } from "@/components/ui/svgs/tiktok";
+import { VSCOIcon } from "@/components/ui/svgs/vsco";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Header({ toggleMenu }: { toggleMenu: () => void }) {
@@ -14,7 +20,34 @@ export default function Header({ toggleMenu }: { toggleMenu: () => void }) {
 					</Button>
 				</div>
 				<div className="justify-self-center">
-					<RakerLogo withVersion />
+					<div className="flex flex-row items-center *:mx-1">
+						<RakerLogo withVersion />
+						<MatchRoute to="/instagram">
+							<Separator orientation="vertical" className="" />
+							<InstagramIcon className="h-4" />
+							<Label>Post</Label>
+						</MatchRoute>
+						<MatchRoute to="/highlight">
+							<Separator orientation="vertical" className="" />
+							<InstagramIcon className="h-4" />
+							<Label>Highlight</Label>
+						</MatchRoute>
+						<MatchRoute to="/story">
+							<Separator orientation="vertical" className="" />
+							<InstagramIcon className="h-4" />
+							<Label>Story</Label>
+						</MatchRoute>
+						<MatchRoute to="/tiktok">
+							<Separator orientation="vertical" className="" />
+							<TikTokIcon className="h-4" />
+							<Label>Post</Label>
+						</MatchRoute>
+						<MatchRoute to="/vsco">
+							<Separator orientation="vertical" className="" />
+							<VSCOIcon className="h-4" />
+							<Label>Post</Label>
+						</MatchRoute>
+					</div>
 				</div>
 				<div className="justify-self-end">
 					<ThemeToggle />
