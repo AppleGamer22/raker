e0a155c0260d9013d2d4e8734232108a2a825c8d
Omri Bornstein <omribor@gmail.com>
2026-04-27 16:58:00 +1000

    form for HistoryPostCategoryForm
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- client/routes/history.tsx
+++ client/routes/history.tsx
@@ -1,25 +1,20 @@
 import { CheckboxGroup } from "@base-ui/react";
 import { useQuery } from "@connectrpc/connect-query";
+import { useForm } from "@tanstack/react-form";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
+import z from "zod";
 
 import { getUserCategories } from "@/buf/raker/v1/raker-RakerServer_connectquery";
 import { CardContent } from "@/components/ui/card";
 import { Checkbox } from "@/components/ui/checkbox";
-import {
-	FieldGroup,
-	FieldLegend,
-	Field,
-	FieldSet,
-	FieldLabel,
-	FieldContent,
-	FieldTitle,
-	FieldSeparator,
-} from "@/components/ui/field";
+import { FieldGroup, FieldLegend, Field, FieldSet, FieldLabel, FieldContent, FieldTitle } from "@/components/ui/field";
+import { Separator } from "@/components/ui/separator";
 import { InstagramIcon } from "@/components/ui/svgs/instagram";
 import { SnapchatIcon } from "@/components/ui/svgs/snapchat";
 import { TikTokIcon } from "@/components/ui/svgs/tiktok";
 import { VSCOIcon } from "@/components/ui/svgs/vsco";
+import { Switch } from "@/components/ui/switch";
 import { useUser } from "@/hooks/user-provider";
 
 export const Route = createFileRoute("/history")({
@@ -101,32 +96,82 @@ function HistoryPostTypeForm() {
 	);
 }
 
+const historyPostCategorySchema = z.object({
+	exclusive: z.boolean().catch(false),
+	categories: z.array(z.string()),
+});
+
 function HistoryPostCategoryForm() {
 	const categoriesQuery = useQuery(getUserCategories, {});
 	const categories = categoriesQuery.data?.categories ?? [];
+	const form = useForm({
+		defaultValues: {
+			exclusive: false,
+			categories: [] as string[],
+		},
+		validators: {
+			onChange: historyPostCategorySchema,
+		},
+	});
 
 	return (
 		<form>
 			<FieldGroup>
 				<FieldSet>
-					<FieldLegend>Post Types</FieldLegend>
-					<FieldGroup data-slot="checkbox-group">
-						<CheckboxGroup className="flex flex-row flex-wrap gap-1 *:w-auto">
-							{categories.map((category) => (
-								<FieldLabel
-									key={`category-${category}`}
-									htmlFor={`category-${category}`}
-									className="max-w-fit"
-								>
+					<FieldLegend>Post Categories</FieldLegend>
+					<FieldGroup className="flex flex-row flex-wrap gap-1 *:w-auto">
+						<form.Field name="exclusive">
+							{(field) => (
+								<FieldLabel htmlFor="category-exclusive" className="max-w-fit">
 									<Field orientation="horizontal">
-										<Checkbox id={`category-${category}`} />
+										<Switch
+											id="category-exclusive"
+											name={field.name}
+											checked={field.state.value}
+											onCheckedChange={field.handleChange}
+										/>
 										<FieldContent>
-											<FieldTitle>{category}</FieldTitle>
+											<FieldTitle>Exclusive</FieldTitle>
 										</FieldContent>
 									</Field>
 								</FieldLabel>
-							))}
-						</CheckboxGroup>
+							)}
+						</form.Field>
+						<Separator orientation="vertical" />
+						<form.Field name="categories" mode="array">
+							{(field) => (
+								<>
+									{categories.map((category) => (
+										<FieldLabel
+											key={`category-${category}`}
+											htmlFor={`category-${category}`}
+											className="max-w-fit"
+										>
+											<Field orientation="horizontal">
+												<Checkbox
+													id={`category-${category}`}
+													name={field.name}
+													checked={field.state.value.includes(category)}
+													onCheckedChange={(checked) => {
+														if (checked) {
+															field.pushValue(category);
+														} else {
+															const index = field.state.value.indexOf(category);
+															if (index > -1) {
+																field.removeValue(index);
+															}
+														}
+													}}
+												/>
+												<FieldContent>
+													<FieldTitle>{category}</FieldTitle>
+												</FieldContent>
+											</Field>
+										</FieldLabel>
+									))}
+								</>
+							)}
+						</form.Field>
 					</FieldGroup>
 				</FieldSet>
 			</FieldGroup>
@@ -147,7 +192,7 @@ function History() {
 	return (
 		<CardContent>
 			<HistoryPostTypeForm />
-			<FieldSeparator />
+			<Separator className="my-2" />
 			<HistoryPostCategoryForm />
 		</CardContent>
 	);
