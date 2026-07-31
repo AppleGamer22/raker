ba32702147d2f74880fb5643fab60b03bca57c5c
Omri Bornstein <omribor@gmail.com>
2026-07-31 19:01:50 +1000

    pgformatter
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- db/queries/users.sql
+++ db/queries/users.sql
@@ -1,49 +1,104 @@
 -- name: UserAdd :exec
-INSERT INTO Users (
-		username,
-		password_hash,
-		instagram_session_id,
-		instagram_user_id,
-		network,
-		categories
-	)
+INSERT INTO Users(
+	username,
+	password_hash,
+	instagram_session_id,
+	instagram_user_id,
+	network,
+	categories)
 VALUES (
-		sqlc.arg(username)::text,
-		sqlc.arg(password_hash)::text,
-		sqlc.arg(instagram_session_id)::text,
-		sqlc.arg(instagram_user_id)::text,
-		'instagram',
-		sqlc.arg(categories)::text []
-	);
+	sqlc.arg(username)::text,
+	sqlc.arg(password_hash)::text,
+	sqlc.arg(instagram_session_id)::text,
+	sqlc.arg(instagram_user_id)::text,
+	'instagram',
+	sqlc.arg(categories)::text[]);
 
 -- name: UserUpdateInstagramSession :exec
-UPDATE Users
-SET instagram_session_id = sqlc.arg(instagram_session_id)::text,
+UPDATE
+	Users
+SET
+	instagram_session_id = sqlc.arg(instagram_session_id)::text,
 	instagram_user_id = sqlc.arg(instagram_user_id)::text,
 	password_hash = sqlc.arg(password_hash)::text
-where username = sqlc.arg(username)::text;
+WHERE
+	username = sqlc.arg(username)::text;
 
 -- name: UserUpdateHash :exec
-UPDATE Users
-SET password_hash = sqlc.arg(password_hash)::text
-where username = sqlc.arg(username)::text;
+UPDATE
+	Users
+SET
+	password_hash = sqlc.arg(password_hash)::text
+WHERE
+	username = sqlc.arg(username)::text;
 
 -- name: UserCategoryAdd :exec
-UPDATE Users
-SET categories = array(
-		select unnest(
-				array_append(categories, sqlc.arg(category)::text)
-			) AS c
-		ORDER BY c
-	)
-where username = sqlc.arg(username)::text;
+UPDATE
+	Users
+SET
+	categories = ARRAY (
+		SELECT
+			unnest(array_append(categories, sqlc.arg(category)::text)) AS c
+		ORDER BY
+			c)
+	WHERE
+		username = sqlc.arg(username)::text;
 
 -- name: UserCategoryRemove :exec
-UPDATE Users
-SET categories = array_remove(categories, sqlc.arg(category)::text)
-where username = sqlc.arg(username)::text;
+UPDATE
+	Users
+SET
+	categories = array_remove(categories, sqlc.arg(category)::text)
+WHERE
+	username = sqlc.arg(username)::text;
 
 -- name: UserGet :one
-SELECT *
-FROM Users
-WHERE username = sqlc.arg(username)::text;
+SELECT
+	*
+FROM
+	Users
+WHERE
+	username = sqlc.arg(username)::text;
+
+-- name: UserGetPasskeysByUsername :many
+SELECT
+	*
+FROM
+	passkeys
+WHERE
+	username = sqlc.arg(username)::text;
+
+-- name: UserCreatePasskey :exec
+INSERT INTO Passkeys(
+	id,
+	username,
+	name,
+	public_key,
+	attestation_type,
+	aaguid,
+	transports)
+VALUES (
+	sqlc.arg(passkey_id)::bytea,
+	sqlc.arg(username)::text,
+	sqlc.arg(name)::text,
+	sqlc.arg(public_key)::bytea,
+	sqlc.arg(attestation_type)::varchar(64),
+	sqlc.arg(aaguid)::bytea,
+	sqlc.arg(transports)::text[]);
+
+-- name: PasskeyUpdateSignCount :exec
+UPDATE
+	Passkeys
+SET
+	sign_count = sign_count + 1
+WHERE
+	id = sqlc.arg(passkey_id)::bytea;
+
+-- name: PasskeyUpdateName :exec
+UPDATE
+	Passkeys
+SET
+	name = sqlc.arg(name)::text
+WHERE
+	id = sqlc.arg(passkey_id)::bytea;
+
--- db/schemas/users.sql
+++ db/schemas/users.sql
@@ -1,14 +1,29 @@
-CREATE TABLE Users (
-	username TEXT PRIMARY KEY,
-	password_hash TEXT NOT NULL,
-	instagram_session_id TEXT NOT NULL,
-	instagram_user_id TEXT NOT NULL,
+CREATE TABLE Users(
+	username text PRIMARY KEY,
+	password_hash text NOT NULL,
+	instagram_session_id text NOT NULL,
+	instagram_user_id text NOT NULL,
 	network network_type NOT NULL,
-	categories TEXT []
+	categories text[]
 );
 
-alter table Users
-add column tiktok_session_id TEXT NOT NULL DEFAULT '';
+ALTER TABLE Users
+	ADD COLUMN tiktok_session_id text NOT NULL DEFAULT '';
 
-alter table Users
-add column tiktok_session_id_guard TEXT NOT NULL DEFAULT '';
+ALTER TABLE Users
+	ADD COLUMN tiktok_session_id_guard text NOT NULL DEFAULT '';
+
+ALTER TABLE Users
+	ADD COLUMN id uuid UNIQUE DEFAULT gen_random_uuid();
+
+CREATE TABLE Passkeys(
+	id bytea PRIMARY KEY,
+	name text NOT NULL,
+	user_id uuid NOT NULL REFERENCES Users(id) ON DELETE CASCADE,
+	public_key bytea NOT NULL,
+	attestation_type varchar(64) NOT NULL,
+	aaguid bytea NOT NULL,
+	sign_count bigint NOT NULL DEFAULT 0,
+	transports text[] NOT NULL DEFAULT '{}'
+);
+
