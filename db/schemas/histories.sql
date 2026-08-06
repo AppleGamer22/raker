848c08b644be98f3abb5a6fe3afbdf4b860c001e
Omri Bornstein <omribor@gmail.com>
2026-08-06 21:52:47 +1000

    query update
    Signed-off-by: Omri Bornstein <omribor@gmail.com>

--- db/schemas/histories.sql
+++ db/schemas/histories.sql
@@ -18,13 +18,3 @@ ALTER TABLE Histories
 ALTER TABLE Histories
 	ADD COLUMN coordinates point DEFAULT NULL;
 
-UPDATE
-	histories
-SET
-	coordinates = NULL;
-
-SELECT DISTINCT
-	coordinates::text
-FROM
-	histories;
-
